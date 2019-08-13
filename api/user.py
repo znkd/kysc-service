9ad398325b9747f9c68739613bc481463948788c
zhangyu <yuzhang180@yirendai.com>
2019-08-13 13:42:25 +0800

    [add]sns/login接口对接👌

--- api/user.py
+++ api/user.py
@@ -6,13 +6,15 @@ import requests
 
 
 class Login(Resource):
-    def post(self):
-        return 'response is ok'
+    def get(self):
+        params = {'phone':'123', 'password':'111111'}
+        r = requests.post('http://47.94.1.127:8010/sns/login', json=params)
+        print(r.status_code)
+        print(r.json())
+        return r.json()
 
 class User(Resource):
     def get(self):
-        r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-        print(r.status_code)
         return 'hello user'
 
 
@@ -20,4 +22,5 @@ if __name__ == "__main__":
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(User, '/user')
+    api.add_resource(Login, '/login')
     app.run(debug=True)
--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 from flask import Flask
 from flask_restful import Api
 from api import User
-
+from api import Login
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(User, '/user')
+api.add_resource(Login, '/login')
 
 if __name__ == "__main__":
     app.run(debug=True)
